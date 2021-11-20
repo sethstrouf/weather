16aea0a2754185fda241bd3e81a1b4d56477fa5a
Seth Strouf <seth.strouf@doane.edu>
2021-11-20 12:53:13 -0600

    Set up layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react'
+import Footer from './Footer';
 import Toggle from './Toggle'
 
 function App() {
   return (
-    <div>
+    <div className="min-h-screen mx-auto text-center">
 
       <h1>Weather App</h1>
 
       <Toggle />
 
+      {/* Mobile - tabs for CURRENT | FORECAST */}
+
+      {/* CURRENT WEATHER */}
+
+      {/* FORECAST WEATHER */}
+
+      <Footer />
 
     </div>
   );
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy;2021 Seth Strouf</p>
+  </footer>
+  )
+}
+
+export default Footer
