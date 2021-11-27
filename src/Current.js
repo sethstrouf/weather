a475070b68167f0ca8667669ead1e96a963c4845
Seth Strouf <seth.strouf@doane.edu>
2021-11-27 10:02:55 -0600

    Adjust icon size slightly

--- src/Current.js
+++ src/Current.js
@@ -6,17 +6,21 @@ const Current = ({location, current}) => {
     <div className="pb-2 bg-rose-50 border-2 border-rose-500 rounded
                     md:w-10/12 xl:w-8/12 2xl:w-6/12 mx-auto">
       <h1 className="hidden">Current</h1>
+
       <p className="serif font-bold pb-1 bg-rose-600 text-gray-50
                     text-xl lg:text-2xl xl:text-3xl">
         {location.name}
       </p>
+
       <p>{current.condition.text}, {current.temp_f}&deg;</p>
+      
       <p className="text-sm sm:text-base lg:text-lg xl:text-xl 2xl:text-2xl">
         {Moment(location.localtime, 'Y-M-D H:m:s').format('LT')}
       </p>
-      <div className="h-14">
+      
+      <div className="h-10 md:h-14">
         <img src={current.condition.icon} alt={current.condition.text} 
-            className="mx-auto" />
+            className="mx-auto h-12 md:h-16" />
       </div>
 
       {/* Flex for trio of information */}
