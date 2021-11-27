a27c00b6444ac3d65d32df609a9b75b58b370c0d
Seth Strouf <seth.strouf@doane.edu>
2021-11-27 04:58:46 -0600

    Create Forecast component

--- src/Data.js
+++ src/Data.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Location from './Location'
 import Current from './Current'
+import Forecast from './Forecast'
 
 const Data = ({data}) => {
   return (
     <div>
       <Location location={data.location} />
       <Current current={data.current} />
+      <Forecast forecast={data.forecast} />
     </div>
   )
 }
---
+++ src/Forecast.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Forecast = ({forecast}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Forecast
